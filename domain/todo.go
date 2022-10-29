fc8d565f66236ddbfdcbb4c73dc7efc8d24e86af
Khafid Prayoga <khafidp@pm.me>
2022-10-29 19:27:04 +0700

    add todo domain

---
+++ domain/todo.go
@@ -0,0 +1,30 @@
+package domain
+
+import "github.com/gofiber/fiber/v2"
+
+// TodoHandler for handle incoming http request with services todo
+type TodoHandler interface {
+	DeleteData() fiber.Handler
+	GetDetail() fiber.Handler
+	GetList() fiber.Handler
+	PostNewData() fiber.Handler
+	UpdateData() fiber.Handler
+}
+
+// TodoUseCase for handle todo services business logic
+type TodoUseCase interface {
+	CreateNewTodo(req string)
+	DeleteExistingTodo(req string)
+	GetAllListTodo(req string)
+	GetDetailTodo(req string)
+	UpdateExistingTodo(req string)
+}
+
+// TodoData to interact with mysql db
+type TodoData interface {
+	DeleteTodo(id string) error
+	DetailTodo(id string) error
+	ListAllTodo() error
+	UpdateTodo(id string) error
+	ValidateTodo(id string) error
+}
