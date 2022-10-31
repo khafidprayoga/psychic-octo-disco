a79d20f873d2f403e59f7aaf563fba841dda01dd
Khafid Prayoga <khafidp@pm.me>
2022-10-31 09:22:43 +0700

    Update main.go

--- main.go
+++ main.go
@@ -4,10 +4,12 @@ import (
 	"github.com/khafidprayoga/psychic-octo-disco/config"
 	"github.com/khafidprayoga/psychic-octo-disco/database"
 	"github.com/khafidprayoga/psychic-octo-disco/registry"
+	"runtime"
 )
 
 // Mount all application backend dependencies
 func main() {
+	runtime.GOMAXPROCS(runtime.NumCPU()) // max performance
 	// Mysql Config
 	mysqlCfg := config.GetMysqlConfig()
 	mysqlClient := database.InitMySqlDB(mysqlCfg)
