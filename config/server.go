ef33bee0390587ec1531ca30e3a136a8491ad46a
Khafid Prayoga <khafidp@pm.me>
2022-12-27 11:14:28 +0700

    test cache and compression

--- config/server.go
+++ config/server.go
@@ -3,6 +3,7 @@ package config
 import (
 	"github.com/bytedance/sonic"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cache"
 	"github.com/gofiber/fiber/v2/middleware/compress"
 	_ "github.com/joho/godotenv/autoload"
 	"os"
@@ -43,7 +44,7 @@ func init() {
 		Server = fiber.New(fiberConfig)
 
 		newCompressor := compress.New(compress.Config{
-			Level: compress.LevelBestCompression,
+			Level: compress.LevelBestSpeed,
 		})
 
 		//newLogger := logger.New(logger.Config{
@@ -58,7 +59,17 @@ func init() {
 		//Use(requestid.New()).
 		//Use(etag.New()).
 		//Use(newLogger)
+		// Initialize default config
+		Server.Use(cache.New())
 
+		// Or extend your config for customization
+		Server.Use(cache.New(cache.Config{
+			Next: func(c *fiber.Ctx) bool {
+				return c.Query("refresh") == "true"
+			},
+			Expiration:   30 * time.Minute,
+			CacheControl: true,
+		}))
 		//Server.Get("/", func(ctx *fiber.Ctx) error {
 		//	return ctx.SendString("Todo list API challenge")
 		//})
